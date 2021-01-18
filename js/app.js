75f4fc421ad809bdc3ea3ff4e3496c62e9f664f8
prinsepunk <planasjuliana@gmail.com>
2021-01-18 13:11:55 -0300

    solucion de height y js de fuente color y backgroundcolor

--- js/app.js
+++ js/app.js
@@ -8,6 +8,13 @@ const bottomText = document.getElementById('bottomText');
 const topCheck = document.getElementById('topCheck');
 const bottomCheck = document.getElementById('bottomCheck');
 const font = document.getElementById('font');
+const fontSize = document.getElementById('fontSize');
+const leftAlign = document.getElementById('leftAlign');
+const centerAlign = document.getElementById('centerAlign');
+const rightAlign = document.getElementById('rightAlign');
+const textColor = document.getElementById('textColor');
+const backColor = document.getElementById('backColor');
+const transparent = document.getElementById('transparent');
 
 topText.addEventListener('keyup', () =>{
     memeTopText.innerHTML = topText.value;
@@ -35,8 +42,48 @@ bottomCheck.addEventListener('click', () =>{
 
 font.addEventListener('change', () =>{
     memeTopText.style.fontFamily = `${font.value}`;
+    memeBottomText.style.fontFamily = `${font.value}`;
 })
 
-font.addEventListener('change', () =>{
-    memeBottomText.style.fontFamily = `${font.value}`;
+fontSize.addEventListener('keyup', () =>{
+    memeTopText.style.fontSize = `${fontSize.value}px`;
+    memeBottomText.style.fontSize = `${fontSize.value}px`;
+})
+
+leftAlign.addEventListener('click', (e) =>{
+    e.preventDefault();
+    memeTopText.style.textAlign = 'left';
+    memeBottomText.style.textAlign = 'left';
+})
+
+centerAlign.addEventListener('click', (e) =>{
+    e.preventDefault();
+    memeTopText.style.textAlign = 'center';
+    memeBottomText.style.textAlign = 'center';
+})
+
+rightAlign.addEventListener('click', (e) =>{
+    e.preventDefault();
+    memeTopText.style.textAlign = 'right';
+    memeBottomText.style.textAlign = 'right';
+})
+
+textColor.addEventListener('change', () =>{
+    memeTopText.style.color = `${textColor.value}`;
+    memeBottomText.style.color = `${textColor.value}`;
+})
+
+backColor.addEventListener('change', () =>{
+    memeTopText.style.backgroundColor = `${backColor.value}`;
+    memeBottomText.style.backgroundColor = `${backColor.value}`;
+})
+
+transparent.addEventListener('click', () =>{
+    if(transparent.checked){
+        memeTopText.style.backgroundColor = 'transparent';
+        memeBottomText.style.backgroundColor = 'transparent';
+    } else{
+        memeTopText.style.backgroundColor = `${backColor.value}`;
+        memeBottomText.style.backgroundColor = `${backColor.value}`;
+    }
 })
